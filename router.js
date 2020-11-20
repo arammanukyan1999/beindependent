e82493a11722aa945f81b008643656821f3f44a9
arammanukyan1999 <aramman1999@gmail.com>
2020-11-20 18:14:26 +0400

    fix

--- router.js
+++ router.js
@@ -3,13 +3,13 @@ const router= expres.Router()
 const path = require('path');
 const RegisterControler=require('./controler/RegisterControler')
 
+
+router.post('/volunteer', RegisterControler.signUp)
+
 router.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
 });
  
-router.post('/volunteer', RegisterControler.signUp)
-
-
 
 
 
